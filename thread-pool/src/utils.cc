2dccacd5505dfe290ebe53f1daec30d96abf5711
linser348@qq.com <root@localhost.localdomain>
2018-06-26 16:10:34 +0800

    set re_useport

--- thread-pool/src/utils.cc
+++ thread-pool/src/utils.cc
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <stdio.h>
 #ifndef MAC
 int add_epoll_fd(int epoll_fd, int fd) {
 	return 1;
@@ -21,7 +22,14 @@ int start_listen(int port){
 		sock_addr.sin_family = AF_INET;
 		sock_addr.sin_port = htons(port);
 		sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		int  reuseport = 1;
 
+                if (setsockopt(fd, SOL_SOCKET, SO_REUSEPORT,
+                               (const void *) &reuseport, sizeof(int))
+                    == -1)
+                {
+			printf("reset error \n");
+                }
 		int ret = bind(fd, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
 		if (ret == 0) {
 			ret = listen(fd, 64);
